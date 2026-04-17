f5eb9a1a453151b914f56b652f4e11f25c7da192
Ilya Makarov <im@ilya-MBP.local>
2026-04-17 11:27:44 +0300

    New version and fixes

--- dev/produce.sh
+++ dev/produce.sh
@@ -2,7 +2,7 @@
 curl -X POST -u "rest-user:rest_password" -H "Content-Type: application/vnd.kafka.avro.v2+json" \
   -H "Accept: application/vnd.kafka.v2+json" \
   --data '{
-    "value_schema": "{\"type\": \"record\", \"name\": \"User\", \"fields\": [{\"name\": \"name2\", \"type\": \"string\"}]}",
+    "value_schema": "{\"type\": \"record\", \"name\": \"User\", \"fields\": [{\"name\": \"name\", \"type\": \"string\"}]}",
     "key_schema": "{\"type\": \"string\"}",
     "records": [
       {"key": "abc","value": {"name": "John Doe"}},
